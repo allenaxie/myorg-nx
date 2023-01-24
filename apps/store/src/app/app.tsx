e9e62795dcd707c8feb2af7d18298284423dcbf8
allenaxie <allenaxie1@gmail.com>
2023-01-24 14:19:44 -0800

    Create two projects. Share UI components and render project graph.

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -1,11 +1,22 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 import NxWelcome from './nx-welcome';
+import { Banner } from '@myorg/common-ui';
+import { exampleProducts } from '@myorg/products';
 
 export function App() {
   return (
     <>
       <NxWelcome title="store" />
+      <Banner text='welcome to the store!'/>
+      <ul>
+        {exampleProducts.map((product) => (
+          <li key={product.id}>
+            <strong>{product.name}</strong>
+            <span>Price: {product.price}</span>
+          </li>
+        ))}
+      </ul>
       <div />
     </>
   );
---
+++ libs/products/src/lib/products.spec.ts
@@ -0,0 +1,8 @@
+import { exampleProducts } from './products';
+
+describe('products', () => {
+  it('should work', () => {
+    const exampleProd = 
+    expect(exampleProducts[0].name).toEqual('Product 1');
+  });
+});
